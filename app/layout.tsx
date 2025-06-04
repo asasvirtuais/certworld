5e0938978aadb9a398633a69ba4f347ef3282f2f
Ícaro Carniello Capobianco <asasvirtuais.dev@gmail.com>
2025-06-04 17:20:49 -0300

    + chakra-ui/react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import { Provider } from '@/components/ui/provider'
 
 export default function RootLayout(props: { children: React.ReactNode }) {
