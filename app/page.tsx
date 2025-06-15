f791e1ae2674c5d5dcf52ba9c793cc2450c2e882
Ícaro Carniello Capobianco <asasvirtuais.dev@gmail.com>
2025-06-15 11:18:09 -0300

    + middleware and auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import { Box } from '@chakra-ui/react'
+import { redirect } from 'next/navigation'
+import auth0 from 'asasvirtuais-auth/auth0.js'
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth0.getSession()
+
+  if (!session?.user)
+    return redirect('/auth/login')
+
   return (
     <Box>
       Use Storybook for the UI
